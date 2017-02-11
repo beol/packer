200e7bcb76bff7e509a555eb12ae27d098590e92
Leo Laksmana <beol@laksmana.com>
2017-02-11 18:43:03 +0800

    work in progress

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,67 +1,33 @@
 node default {
-  class { '::selinux':
-    mode => 'disabled',
-  }
+  hiera_include('classes')
 
-  sysctl { [ 'net.ipv6.conf.all.disable_ipv6', 'net.ipv6.conf.default.disable_ipv6' ]:
-    ensure => present,
-    value  => '1',
-  }
+  #$sysctls = hiera('sysctls')
+  #create_resources('sysctl', $sysctls)
 
-  sshd_config { 'AddressFamily':
-    ensure => present,
-    value  => 'inet',
-  }
+  #$sshd_configs = hiera('sshd_configs')
+  #create_resources('sshd_config', $sshd_configs)
 
-  sshd_config { 'UseDNS':
-    ensure => present,
-    value  => 'no',
-  }
+  #$packages = hiera('packages')
+  #create_resources('package', $packages)
 
-  service { [ 'iptables', 'ip6tables' ]:
-    ensure => stopped,
-    enable => false,
-  }
+  #$services = hiera('services')
+  #create_resources('service', $services)
 
-  include ::sudo
+  #$dnsmasq_conf = hiera('dnsmasq::conf')
+  #create_resources('dnsmasq::conf', $dnsmasq_conf)
 
-  sudo::conf { 'require-tty':
-    priority => 0,
-    content  => 'Defaults !requiretty',
-  }
+  #file { '/etc/dhcp/dhclient-eth0.conf':
+    #ensure => present,
+  #} ->
+  #file_line { 'prepend domain-name-servers':
+    #ensure => present,
+    #path   => '/etc/dhcp/dhclient-eth0.conf',
+    #line   => 'prepend domain-name-servers 127.0.0.1;',
+  #}
 
-  class { '::ntp':
-    servers    => [ '0.pool.ntp.org', '1.pool.ntp.org' ],
-    interfaces => [ '127.0.0.1' ],
-    restrict   => [
-      'default ignore',
-      '127.0.0.1',
-      '0.pool.ntp.org nomodify notrap nopeer noquery',
-      '1.pool.ntp.org nomodify notrap nopeer noquery'
-    ],
-  }
+  $users = hiera('users')
+  create_resources('user', $users)
 
-  include ::dnsmasq
-
-  dnsmasq::conf { 'interface':
-    ensure  => present,
-    content => 'interface=lo
-listen-address=127.0.0.1
-bind-interfaces
-',
-    prio    => 0,
-  }
-
-  package { [ 'bind-utils', 'vim-enhanced' ]:
-    ensure => present,
-  }
-
-  file { '/etc/dhcp/dhclient-eth0.conf':
-    ensure => present,
-  } ->
-  file_line { 'prepend domain-name-servers':
-    ensure => present,
-    path   => '/etc/dhcp/dhclient-eth0.conf',
-    line   => 'prepend domain-name-servers 127.0.0.1;',
-  }
+  $ssh_authorized_keys = hiera('ssh_authorized_keys')
+  create_resources('ssh_authorized_key', $ssh_authorized_keys)
 }
